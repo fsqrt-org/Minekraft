9b7d9a06e2daa572455210dd2f41a9ab220a4540
Max Bossing <info@maxbossing.de>
2025-07-20 23:19:59 +0200

    feat: GPR Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.kotlinx.serialization)
+    `maven-publish`
 }
 
 repositories {
     mavenCentral()
 }
 
-allprojects {
+subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "kotlinx-serialization")
+    apply(plugin = "maven-publish")
+    apply(plugin = "java-library")
 
     group = "org.fsqrt.minekraft"
     version = "1.0.0"
@@ -33,5 +36,53 @@ allprojects {
                 kotlin.srcDirs("src")
             }
         }
+    }
+
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components.findByName("java"))
+
+                versionMapping {
+                    usage("java-api") {
+                        fromResolutionOf("runtimeClasspath")
+                    }
+                    usage("java-runtime") {
+                        fromResolutionResult()
+                    }
+                }
+
+                groupId = "org.fsqrt.minekraft"
+                artifactId = project.name.lowercase()
+                version = project.version.toString()
+
+                pom {
+                    name.set(project.name)
+                    description.set("")
+                    url.set("https://github.com/fsqrt-org/Minekraft")
+                    developers {
+                        developer {
+                            id.set("maxbossing")
+                            name.set("Max Bossing")
+                            email.set("max@bossi.ng")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            mavenLocal()
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/fsqrt-org/Minekraft")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+
     }
 }
